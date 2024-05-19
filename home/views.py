6dae1293dbaa32ebab34ad43d3032e59282cab54
David Hitchen <hitchen28963@yahoo.com>
2024-05-19 15:21:49 +0000

    added form to home view to ensure functionality

--- home/views.py
+++ home/views.py
@@ -1,5 +1,35 @@
 from django.shortcuts import render
+from real_estate.models import Property
+from real_estate.forms import PropertySearchForm
 
-# Create your views here.
 def home(request):
-    return render(request, 'home/index.html')
+    form = PropertySearchForm(request.GET)
+    properties = Property.objects.all()
+
+    if form.is_valid():
+        if form.cleaned_data.get('search'):
+            properties = properties.filter(location__icontains=form.cleaned_data['search'])
+        if form.cleaned_data.get('location'):
+            properties = properties.filter(location__icontains=form.cleaned_data['location'])
+        if form.cleaned_data.get('property_type') and form.cleaned_data['property_type'] != 'any':
+            properties = properties.filter(property_type=form.cleaned_data['property_type'])
+        if form.cleaned_data.get('bedrooms_min'):
+            properties = properties.filter(bedrooms__gte=form.cleaned_data['bedrooms_min'])
+        if form.cleaned_data.get('bedrooms_max'):
+            properties = properties.filter(bedrooms__lte=form.cleaned_data['bedrooms_max'])
+        if form.cleaned_data.get('price_min'):
+            properties = properties.filter(price__gte=form.cleaned_data['price_min'])
+        if form.cleaned_data.get('price_max'):
+            properties = properties.filter(price__lte=form.cleaned_data['price_max'])
+        if form.cleaned_data.get('garden'):
+            properties = properties.filter(garden=form.cleaned_data['garden'])
+        if form.cleaned_data.get('parking'):
+            properties = properties.filter(parking=form.cleaned_data['parking'])
+        if form.cleaned_data.get('pets_allowed'):
+            properties = properties.filter(pets_allowed=form.cleaned_data['pets_allowed'])
+    
+    context = {
+        'form': form,
+        'properties': properties
+    }
+    return render(request, 'home/index.html', context)
