041781b951ad820bd0d749ad9a51ebf004115b56
David Hitchen <hitchen28963@yahoo.com>
2024-05-19 16:54:24 +0000

    Amended forms.py and property_sale.html & property_rent.html to resolve filtering by location issue

--- real_estate/forms.py
+++ real_estate/forms.py
@@ -2,13 +2,13 @@ from django import forms
 from .models import Property
 
 class PropertySearchForm(forms.Form):
-    search = forms.CharField(required=False, widget=forms.TextInput(attrs={'placeholder': 'Search...'}))
-    property_type = forms.ChoiceField(choices=[('', 'Any')] + Property.PROPERTY_TYPE_CHOICES, required=False)
-    bedrooms_min = forms.IntegerField(required=False, min_value=0)
-    bedrooms_max = forms.IntegerField(required=False, min_value=0)
-    price_min = forms.IntegerField(required=False, min_value=0)
-    price_max = forms.IntegerField(required=False, min_value=0)
+    search = forms.CharField(required=False, widget=forms.TextInput(attrs={'placeholder': 'Enter location'}))
+    location = forms.CharField(required=False, widget=forms.TextInput(attrs={'placeholder': 'Enter location'}))
+    property_type = forms.ChoiceField(choices=[('any', 'Any')] + list(Property.PROPERTY_TYPE_CHOICES), required=False)
+    bedrooms_min = forms.IntegerField(required=False)
+    bedrooms_max = forms.IntegerField(required=False)
+    price_min = forms.DecimalField(required=False)
+    price_max = forms.DecimalField(required=False)
     garden = forms.BooleanField(required=False)
     parking = forms.BooleanField(required=False)
     pets_allowed = forms.BooleanField(required=False)
-    transaction_type = forms.ChoiceField(choices=[('sale', 'For Sale')], required=True)  # Add this line for sale page
