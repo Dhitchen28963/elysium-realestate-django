1342b5af18b6cc3f49bab339a633c7fa6dceefe7
David Hitchen <hitchen28963@yahoo.com>
2024-05-20 21:27:42 +0000

    Added models for testimonials app

---
+++ testimonials/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Testimonial(models.Model):
+    author = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.author
