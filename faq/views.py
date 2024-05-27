484168fdbe81ffda0414ad09dacf36a024cefea3
David Hitchen <hitchen28963@yahoo.com>
2024-05-27 12:17:57 +0000

    amendd the faqlist and faqdetail classes

--- faq/views.py
+++ faq/views.py
@@ -1,10 +1,22 @@
-from django.views.generic import ListView, DetailView
+from django.shortcuts import render, get_object_or_404
+from django.views import View
 from .models import FAQ
+from .forms import CommentForm
 
-class FAQListView(ListView):
-    model = FAQ
-    template_name = 'faq/faq_list.html'
+class FAQList(View):
+    def get(self, request, *args, **kwargs):
+        faqs = FAQ.objects.filter(status='published').order_by('-created_on')
+        return render(request, 'faq/faq_list.html', {'faqs': faqs})
 
-class FAQDetailView(DetailView):
-    model = FAQ
-    template_name = 'faq/faq_detail.html'
+class FAQDetail(View):
+    def get(self, request, slug, *args, **kwargs):
+        faq = get_object_or_404(FAQ, slug=slug)
+        comments = faq.comments.filter(approved=True)
+        new_comment = None
+        comment_form = CommentForm()
+        return render(request, 'faq/faq_detail.html', {
+            'faq': faq,
+            'comments': comments,
+            'new_comment': new_comment,
+            'comment_form': comment_form
+        })
