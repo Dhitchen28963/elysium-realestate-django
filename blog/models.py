e6d0a1a72df24df0450738996eb0e54f286e9cee
David Hitchen <hitchen28963@yahoo.com>
2024-05-27 12:10:25 +0000

    Changed class from blog to post and added an additional comment class

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-class Blog(models.Model):
-    title = models.CharField(max_length=200)
+class Post(models.Model):
+    STATUS_CHOICES = [
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    ]
+
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    featured_image = models.ImageField(upload_to='blog_images/', default='default.jpg')
     content = models.TextField()
-    author = models.CharField(max_length=100)
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+    excerpt = models.TextField(max_length=500, blank=True)
 
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.CharField(max_length=255)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'Comment by {self.author} on {self.post}'
