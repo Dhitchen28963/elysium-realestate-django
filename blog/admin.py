878f1a9bdf4dc27af1e86efb0fbec445028be4fe
David Hitchen <hitchen28963@yahoo.com>
2024-05-27 12:08:00 +0000

    Added comment admin class to allow users to comment on blog posts

--- blog/admin.py
+++ blog/admin.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
-from .models import Blog
+from .models import Post, Comment
 
-@admin.register(Blog)
-class BlogAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'created_at')
-    search_fields = ('title', 'author')
-    list_filter = ('created_at',)
-    ordering = ('-created_at',)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author', 'created_on', 'status')
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('author', 'post', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ['author', 'body']
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
