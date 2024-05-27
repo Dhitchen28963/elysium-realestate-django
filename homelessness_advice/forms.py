b25880128f514cf68cf3b781bd5c6d5354f2ceae
David Hitchen <hitchen28963@yahoo.com>
2024-05-27 12:21:34 +0000

    Added forms.py

---
+++ homelessness_advice/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from .models import Post
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'slug', 'author', 'featured_image', 'content', 'status', 'excerpt')
