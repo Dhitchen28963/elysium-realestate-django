f6603de51920fac77c53db5159ad0a472903ff46
David Hitchen <hitchen28963@yahoo.com>
2024-05-27 12:15:35 +0000

    Added commentadmin class

--- faq/admin.py
+++ faq/admin.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
-from .models import FAQ
+from .models import FAQ, Comment
 
-@admin.register(FAQ)
 class FAQAdmin(admin.ModelAdmin):
-    list_display = ('question',)
-    search_fields = ('question',)
+    list_display = ('title', 'slug', 'author', 'created_on', 'status')
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('author', 'post', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ['author', 'body']
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
+
+admin.site.register(FAQ, FAQAdmin)
+admin.site.register(Comment, CommentAdmin)
