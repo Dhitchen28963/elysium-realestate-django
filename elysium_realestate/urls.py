cc89638d8f695204cfda8096a166b681d2aae05c
David Hitchen <hitchen28963@yahoo.com>
2024-06-02 10:49:44 +0000

    Removed incorrectly placed urls paths from main urls.py page to the real_estate urls.py page

--- elysium_realestate/urls.py
+++ elysium_realestate/urls.py
@@ -16,15 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from real_estate import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path("accounts/", include("allauth.urls")),
     path('summernote/', include('django_summernote.urls')),
-    path('property-sale/', include('real_estate.urls'), name='property_sale'),
-    path('property-rent/', include('real_estate.urls'), name='property_rent'),
     path('real_estate/', include('real_estate.urls')),
     path('student_accommodation/', include('student_accommodation.urls')),
     path('property_guides/', include('property_guides.urls')),
@@ -32,16 +29,4 @@ urlpatterns = [
     path('testimonials/', include('testimonials.urls')),
     path('faq/', include('faq.urls')),
     path('blog/', include('blog.urls')),
-    path('add-to-favorites/<int:property_id>/', views.add_to_favorites, name='add_to_favorites'),
-    path('schedule-viewing/<int:slot_id>/', views.schedule_viewing, name='schedule_viewing'),
-    path('view-property-slots/<int:property_id>/', views.view_property_slots, name='view_property_slots'),
-    path('contact-property/<int:property_id>/', views.contact_property, name='contact_property'),
-    path('property-slots/<int:property_id>/', views.view_property_slots, name='view_property_slots'),
-    path('favorites/', views.view_favorites, name='view_favorites'),
-    path('remove-from-favorites/<int:property_id>/', views.remove_from_favorites, name='remove_from_favorites'),
-    path('request-custom-viewing/<int:property_id>/', views.request_custom_viewing, name='request_custom_viewing'),
-    path('profile/', views.ProfileView.as_view(), name='profile'),
-    path('view-messages/', views.MessagesView.as_view(), name='view_messages'),
-    path('view-search-alerts/', views.SearchAlertsView.as_view(), name='view_search_alerts'),
-    path('account-settings/', views.AccountSettingsView.as_view(), name='account_settings'),
 ]
