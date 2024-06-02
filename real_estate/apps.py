2cc70221723d9d700aece37da5ab5bed7fd5a148
David Hitchen <hitchen28963@yahoo.com>
2024-06-02 10:44:50 +0000

    Included signals.py to apps.py file for the create property alerts function

--- real_estate/apps.py
+++ real_estate/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
 
-
 class RealEstateConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
     name = 'real_estate'
+
+    def ready(self):
+        import real_estate.signals
