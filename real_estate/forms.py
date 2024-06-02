571a7315467914118e3fc3862eb7a46435832231
David Hitchen <hitchen28963@yahoo.com>
2024-06-02 10:45:51 +0000

    Added savedSearchForm class to forms.py

--- real_estate/forms.py
+++ real_estate/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Property, ViewingAppointment
+from .models import Property, ViewingAppointment, SavedSearch
 
 class PropertySearchForm(forms.Form):
     search = forms.CharField(required=False, widget=forms.TextInput(attrs={'placeholder': 'Enter location'}))
@@ -13,8 +13,32 @@ class PropertySearchForm(forms.Form):
     parking = forms.BooleanField(required=False)
     pets_allowed = forms.BooleanField(required=False)
 
-
 class ViewingAppointmentForm(forms.ModelForm):
     class Meta:
         model = ViewingAppointment
         fields = ['name', 'contact', 'email', 'preferred_date', 'preferred_time', 'message']
+
+class SavedSearchForm(forms.ModelForm):
+    class Meta:
+        model = SavedSearch
+        fields = [
+            'search_name', 'location', 'property_type', 'bedrooms_min', 'bedrooms_max',
+            'bathrooms_min', 'bathrooms_max', 'price_min', 'price_max', 'garden', 'parking',
+            'pets_allowed', 'furnished_type'
+        ]
+        widgets = {
+            'property_type': forms.Select(choices=[
+                ('detached-houses', 'Detached houses'),
+                ('semi-detached-houses', 'Semi-detached houses'),
+                ('terraced-houses', 'Terraced houses'),
+                ('mobile-park-homes', 'Mobile / Park homes'),
+                ('boats', 'Boats'),
+                ('flats-apartments', 'Flats / Apartments'),
+                ('bungalows', 'Bungalows'),
+                ('land', 'Land'),
+                ('commercial-property', 'Commercial Property'),
+                ('hmo', 'HMO\'s')
+            ]),
+            'price_min': forms.NumberInput(),
+            'price_max': forms.NumberInput(),
+        }
