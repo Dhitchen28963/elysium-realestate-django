6c329fbda44dd7c78c4b695e7b750f268e23e745
David Hitchen <hitchen28963@yahoo.com>
2024-07-25 18:30:29 +0000

    Added unit tests for comment form including valid and invalid data and added error handling

---
+++ blog/tests/test_forms.py
@@ -0,0 +1,22 @@
+from django.test import TestCase
+from blog.forms import CommentForm
+from blog.models import Comment
+
+class TestCommentForm(TestCase):
+
+    def test_comment_form_valid_data(self):
+        form = CommentForm(data={
+            'body': 'This is a test comment'
+        })
+
+        self.assertTrue(form.is_valid())
+
+    def test_comment_form_empty_data(self):
+        form = CommentForm(data={})
+
+        self.assertFalse(form.is_valid())
+        self.assertEqual(len(form.errors), 1)
+
+    def test_comment_form_fields(self):
+        form = CommentForm()
+        self.assertEqual(list(form.fields), ['body'])
