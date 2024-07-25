ebaa14a9dd200df0854b685adcb8b26482c45845
David Hitchen <hitchen28963@yahoo.com>
2024-07-25 20:58:30 +0000

    Add form tests for PostForm in homelessness_advice app

---
+++ homelessness_advice/tests/test_forms.py
@@ -0,0 +1,37 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+from homelessness_advice.forms import PostForm
+from homelessness_advice.models import Post
+
+class PostFormTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = User.objects.create_user(username='user', password='password')
+
+    def test_valid_form(self):
+        form_data = {
+            'title': 'Test Title',
+            'slug': 'test-title',
+            'author': self.user.id,
+            'featured_image': 'sample-image.jpg',
+            'content': 'Test content',
+            'status': 'published',
+            'excerpt': 'Test excerpt'
+        }
+        form = PostForm(data=form_data)
+        self.assertTrue(form.is_valid())
+
+    def test_invalid_form(self):
+        form_data = {
+            'title': '',
+            'slug': '',
+            'author': self.user.id,
+            'featured_image': 'sample-image.jpg',
+            'content': 'Test content',
+            'status': 'published',
+            'excerpt': 'Test excerpt'
+        }
+        form = PostForm(data=form_data)
+        self.assertFalse(form.is_valid())
+        self.assertIn('title', form.errors)
+        self.assertIn('slug', form.errors)
