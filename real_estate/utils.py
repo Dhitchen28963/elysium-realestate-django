1c1e36eb369c74fd418288608daaf507e55441fc
David Hitchen <hitchen28963@yahoo.com>
2024-07-29 17:11:58 +0000

    Ensure the style attribute for the span tags is correctly allowed in real_estate app and added safe to ensure correct rendering

---
+++ real_estate/utils.py
@@ -0,0 +1,41 @@
+import re
+import bleach
+from html import unescape
+
+def replace_font_with_span(html):
+    # Replace <font color="#...">...</font> with <span style="color:...;">...</span>
+    font_open_tag_pattern = re.compile(r'<font\s+color="([^"]+)">')
+    font_close_tag_pattern = re.compile(r'</font>')
+    
+    # Replace opening font tags
+    html = font_open_tag_pattern.sub(r'<span style="color:\1;">', html)
+    # Replace closing font tags
+    html = font_close_tag_pattern.sub('</span>', html)
+    
+    return html
+
+def clean_html_content(html):
+    # Add debug print statements
+    
+    # Unescape HTML entities
+    html = unescape(html)
+
+    allowed_tags = [
+        'p', 'b', 'i', 'u', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6',
+        'span', 'ul', 'ol', 'li', 'strong', 'br', 'em', 'blockquote', 'a'
+    ]
+    allowed_attrs = {
+        'span': ['style'],
+        'a': ['href', 'title', 'style'],
+        '*': ['style']
+    }
+
+    allowed_styles = ['color']
+
+    # Replace <font> tags with <span> tags
+    html = replace_font_with_span(html)
+
+    # Sanitize the HTML
+    cleaned_html = bleach.clean(html, tags=allowed_tags, attributes=allowed_attrs, styles=allowed_styles, strip=False)
+
+    return cleaned_html
