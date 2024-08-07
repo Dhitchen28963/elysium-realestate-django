49a4481799e64346bc9099fc2c65718c743e6aea
David Hitchen <hitchen28963@yahoo.com>
2024-08-07 23:05:46 +0000

    Update blog URLs to reflect changes in views and model

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,9 @@
-from django.urls import path, include
-from .views import BlogList, BlogDetail, BlogCommentEdit, delete_blog_comment
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('', BlogList.as_view(), name='blog_list'),
-    path('<slug:slug>/', BlogDetail.as_view(), name='blog_detail'),
-    path('comment/<int:comment_id>/edit/', BlogCommentEdit.as_view(), name='blog_comment_edit'),
-    path('comment/<int:comment_id>/delete/', delete_blog_comment, name='blog_comment_delete'),
+    path('', views.BlogList.as_view(), name='blog_list'),
+    path('<slug:slug>/', views.BlogDetail.as_view(), name='blog_detail'),
+    path('comment/edit/<int:comment_id>/', views.BlogCommentEdit.as_view(), name='comment_edit'),
+    path('comment/delete/<int:comment_id>/', views.delete_blog_comment, name='comment_delete'),
 ]
